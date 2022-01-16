b40aa8d3fbed075f5297b1cd36f8b865ed26ba26
Tom Riley <mail@thomasriley.ca>
2022-01-16 13:46:27 -0500

    Change text

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no todo items yet! You can add one above!</p>
+                <p className="text-center">You have no todo items yet! Why not add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
